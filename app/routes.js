b5f5897a5239293534d0cb5b7ab76bd9385b13fb
Luis Peña <pezu.luis01@gmail.com>
2021-06-25 13:04:32 -0500

    refactor:: Put out the controller in file

--- app/routes.js
+++ app/routes.js
@@ -1,12 +1,11 @@
+import HomeCtrl from './controller'
+
 export default function routing ($urlRouterProvider, $stateProvider) {
     'ngInject';
     $stateProvider.state('home', {
         url: '/',
-        template: '<h1 class="main">{{header}}</h1>',
-        controller: function($scope) {
-            'ngInject';
-            $scope.header = 'Header application';
-        },
+        template: '<h1 class="main">{{header}}</h1><h2>{{Ctrl.greet}}</h2>',
+        controller: HomeCtrl,
         controllerAs: 'Ctrl'
     })
     $urlRouterProvider.otherwise('/');
