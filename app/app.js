57d0856624cea431317f627011759032b17e9857
Luis Peña <pezu.luis01@gmail.com>
2021-06-25 12:05:32 -0500

    feat:: Add the main module of the application

--- app/app.js
+++ app/app.js
@@ -1,5 +1,12 @@
 import './style.css';
+import angular from 'angular';
 
 if (module.hot) { // Indicar el uso HotModuleReplacement
     module.hot.accept();
 }
+
+angular.module('app', [])
+.controller('Ctrl', function ($scope) {
+    'ngInject';
+    $scope.header = 'Header application'
+})
